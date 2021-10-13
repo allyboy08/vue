20823d926761382395c7ca6e25e523f5cf03417e
allyboy08 <alexsassman65@gmail.com>
2021-10-13 14:38:40 +0200

    change

--- app.js
+++ app.js
@@ -1,27 +1,26 @@
 const app = Vue.createApp({
     data() {
         return {
+            url: 'https://alexsassman-portfolio.netlify.app/',
             showBooks: true,
-            title: 'the final empire',
-            author: 'Brandon Sanderson',
-            age: 45,
-            x: 0,
-            y: 0
+            books: [
+                {title: 'name of the wind', author: 'patrick rothfuss', img: 'assets/1.jpg', isFav: true},
+                {title: 'the way of kings', author: 'brandon sanderson', img: 'assets/2.jpg', isFav: false},
+                {title: 'the final empire', author: 'brandon sanderson', img: 'assets/3.jpg', isFav: true}
+            ]
         }
     },
     methods: {
         toggleShowBooks() {
             this.showBooks = !this.showBooks
         },
-        handleEvent(e, data) {
-            console.log(e, e.type)
-            if (data) {
-                console.log(data)
-            }
-        },
-        handleMousemove(e) {
-            this.x = e.offsetX
-            this.y = e.offsetY
+        toggleFav(book) {
+            book.isFav = !book.isFav
+        }
+    },
+    computed: {
+        filteredBooks() {
+            return this.books.filter((book) => book.isFav)
         }
     }
 })
